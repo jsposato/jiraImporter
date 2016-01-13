5df8c73e1eceed4d1700160b8f45ca0e5dd8505a
John Sposato <jsposato@mobiquityinc.com>
2016-01-12 22:19:00 -0500

    Added dotEnv library, sample env file

--- importCSV.php
+++ importCSV.php
@@ -1,4 +1,18 @@
 <?php
+include_once('vendor/autoload.php');
 
 use Curl\Curl;
+use josegonzalez\Dotenv\Loader;
+
+try {
+    $env = new Loader('.env');
+
+} catch (Exception $e) {
+    print $e->getMessage();
+    exit;
+}
+$env->parse();
+$blah = $env->toArray();
+
+print_r($blah);
 
